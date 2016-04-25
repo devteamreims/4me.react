b26545523dfe3426a459a5502cd6a2509d069f79
Benjamin Beret <benjamin.beret@gmail.com>
2016-04-25 17:46:51 +0200

    Prepare status in XMAN

--- src/xman/reducers/status/index.js
+++ src/xman/reducers/status/index.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import socket from './socket';
 import flightList from './flight-list';
 import fetcherServices from './fetcher-services';
+import positionService from './position-service';
 
 const statusReducer = combineReducers({
   socket,
   flightList,
   fetcherServices,
+  positionService,
 });
 
 export default statusReducer;
--- src/xman/selectors/status.js
+++ src/xman/selectors/status.js
@@ -3,15 +3,43 @@ import p from './prefix';
 
 export const getRaw = (state) => _.get(p(state), 'status', {});
 
-const getSocketStatus = (state) => ({
-  status: 'normal',
-  name: 'XMAN Socket',
-  description: 'Realtime socket connection to XMAN backend',
-});
+const getSocketStatus = (state) => _.get(getRaw(state), 'socket');
+const getPrettySocketStatus = (state) => {
+  return {
+    ...getSocketStatus(state),
+    name: 'XMAN Socket',
+    description: 'Realtime socket connection to XMAN backend',
+  };
+};
+
+const getFetchersStatuses = (state) => _.get(getRaw(state), 'fetcherServices');
+const getPrettyFetchersStatuses = (state) => {
+  const prettyData = fetcher => ({
+    name: `XMAN Fetcher : ${fetcher}`,
+    description: `${fetcher} XMAN data source`,
+  });
+
+  return _.map(getFetchersStatuses(state), (value, key) => ({
+    ...value,
+    ...prettyData(key),
+  }));
+};
+
+const getPositionServiceStatus = (state) => _.get(getRaw(state), 'positionService');
+const getPrettyPositionServiceStatus = (state) => {
+  return {
+    ...getPositionServiceStatus(state),
+    name: 'XMAN Flight positions',
+    description: 'Data source for flight positions',
+  };
+};
+
 
 export const getStatus = (state) => {
   const items = [
-    getSocketStatus(state),
+    getPrettySocketStatus(state),
+    ...getPrettyFetchersStatuses(state),
+    getPrettyPositionServiceStatus(state),
   ];
 
   return {
