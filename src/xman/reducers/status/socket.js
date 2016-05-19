bb0492689edf465d65c7529f08ad1e0b66b6b82d
Benjamin Beret <benjamin.beret@gmail.com>
2016-05-19 16:26:22 +0200

    XMAN Socket default status to error (prior to connection)

--- src/xman/reducers/status/socket.js
+++ src/xman/reducers/status/socket.js
@@ -4,14 +4,14 @@ import {
 } from '../../actions/socket';
 
 const defaultState = {
-  status: 'normal',
+  status: 'error',
   lastUpdated: Date.now(),
 };
 
 export default function socketStatusReducer(state = defaultState, action) {
   switch(action.type) {
     case SOCKET_CONNECTED:
-      return Object.assign({}, defaultState, {lastUpdated: Date.now()});
+      return Object.assign({}, defaultState, {status: 'normal', lastUpdated: Date.now()});
     case SOCKET_DISCONNECTED:
       return Object.assign({}, state, {status: 'error', lastUpdated: Date.now()});
   }
