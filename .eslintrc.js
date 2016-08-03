6c501c08f9438833b00da07a89060d00743318f8
Benjamin Beret <benjamin.beret@gmail.com>
2016-08-03 10:29:11 +0200

    Better eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,13 @@ module.exports = {
   "parser": "babel-eslint",
   "plugins": ["react"],
   "rules": {
-    "react/prop-types": 1,
-    "import/imports-first": 1,
+    "react/prop-types": 0,
+    "import/imports-first": 0,
     "object-curly-spacing": 0,
     "react/jsx-filename-extension": 0,
+    "react/prefer-stateless-function": 1,
+    "max-len": ["error", 120],
+    "no-unused-vars": 1,
+    "keyword-spacing": 1,
   }
 };
