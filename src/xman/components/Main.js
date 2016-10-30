78cdc3400e6b4284bd0cfdd46912ee946c52b274
Benjamin Beret <benjamin.beret@gmail.com>
2016-10-30 17:09:40 +0100

    Unconnect a component, feed it props from parent instead of store

--- src/xman/components/Main.js
+++ src/xman/components/Main.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-
 import { connect } from 'react-redux';
+
+import R from 'ramda';
 
 import Divider from 'material-ui/Divider';
 
@@ -14,19 +15,37 @@ class XmanRoot extends Component {
     const {
       shouldDisplayMessage,
       shouldDisplayList,
+      client,
+      sectors,
     } = this.props;
+
+    // Show filter controls only when we have sectors
+    const showFilterControl = !R.isEmpty(sectors);
+    // Show advanced controls only when we're a supervisor or a developer
+    const showSupervisorControl = R.pipe(
+      R.propOr(null, 'type'),
+      R.equals('supervisor'),
+    )(client) || process.env.NODE_ENV === 'development';
+
+    const showFlightListControl = showFilterControl || showSupervisorControl;
 
     return (
       <div>
-        <FlightListControls />
-        <Divider />
+        {showFlightListControl && [
+          <FlightListControls
+            key="flight-list-controls-0"
+            showFilterControl={showFilterControl}
+            showSupervisorControl={showSupervisorControl}
+          />,
+          <Divider key="flight-list-controls-1" />,
+        ]}
         {shouldDisplayMessage && [
           <StatusMessage
-            key={0}
+            key="status-message-0"
             style={{textAlign: 'center', margin: 0, padding: 10}}
           />,
           <Divider
-            key={1}
+            key="status-message-1"
           />,
         ]
         }
@@ -46,8 +65,4 @@ const mapStateToProps = (state) => ({
   shouldDisplayList: shouldDisplayList(state),
 });
 
-const mapDispatchToProps = {
-};
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(XmanRoot);
+export default connect(mapStateToProps)(XmanRoot);
