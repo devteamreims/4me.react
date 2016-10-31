77aa7fe3a4a5a4e551c16e350f9cec249b9ea1c2
Benjamin Beret <benjamin.beret@gmail.com>
2016-10-31 12:20:39 +0100

    Improve visual style of widgets

--- src/core/components/Dashboard/Widget.js
+++ src/core/components/Dashboard/Widget.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
 import { GridTile } from 'material-ui/GridList';
-import { Link } from 'react-router';
+import Paper from 'material-ui/Paper';
 
 const styles = {
   gridTile: {
-    flexGrow: 1,
+    margin: 10,
   },
   gridTileChildren: {
     maxHeight: '100%',
     overflowY: 'auto',
+    padding: 10,
   },
 };
 
@@ -17,17 +18,27 @@ const Widget = ({
   cols = 1,
   children,
   title,
-}) => (
-  <GridTile
-    style={styles.gridTile}
-    cols={cols}
-    title={title}
-  >
-    <div style={styles.gridTileChildren}>
-      {children}
-    </div>
-  </GridTile>
-);
+}) => {
+  // If we have a title, we must add some padding in our div to allow scroll all the way down
+  const innerStyle = Object.assign({}, styles.gridTileChildren);
+
+  if(title) {
+    Object.assign(innerStyle, {paddingBottom: 48});
+  }
+
+  return (
+    <GridTile
+      style={styles.gridTile}
+      cols={cols}
+      title={title}
+      containerElement={Paper}
+    >
+      <div style={innerStyle}>
+        {children}
+      </div>
+    </GridTile>
+  );
+};
 
 
 export default Widget;
