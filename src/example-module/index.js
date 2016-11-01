94d2f483f8c23844174771a6c6071717097c1a53
Benjamin Beret <benjamin.beret@gmail.com>
2016-11-01 13:07:39 +0100

    Adjust documentation to new API

--- src/example-module/index.js
+++ src/example-module/index.js
@@ -1,7 +1,4 @@
-import MenuButtonComponent from './components/MenuButton';
-import MainComponent from './components/Main';
-import StatusComponent from './components/Status';
-import WidgetComponent from './components/Widget';
+
 
 import exampleReducer from './reducer';
 
@@ -9,6 +6,11 @@ import { p } from './selectors';
 
 export const name = 'exampleModule';
 export const uri = '/example-module';
+
+import MenuButtonComponent from './components/MenuButton';
+import MainComponent from './components/Main';
+import StatusComponent from './components/Status';
+import WidgetComponent from './components/Widget';
 
 export const MenuButton = MenuButtonComponent;
 export const Main = MainComponent;
@@ -31,18 +33,23 @@ export function getStatusString(state) {
  * This is the main interface for 4ME UI modules
  * A 4ME Module must export a set of things to integrate with 4ME UI framework
  *
- * * MenuButtonComponent (React Component) : this will be rendered in the left navigation bar
+ * * name (string) : A key to identify this module. This key will be used as a state slice
+ * in case you wish to integrate with redux
+ *
+ * * uri (string) : A string to configure the main router, must be prefixed by '/'
+ *
+ * * MenuButton (React Component) : this will be rendered in the left navigation bar
  * This component is rendered at anytime. This is a good place to initialize a 4ME module and
  * perform bootstrapping stuff.
  *
- * * WidgetComponent (React Component) : this will be rendered on the Dashboard page
+ * * Widget (React Component) : this will be rendered on the Dashboard page
  * This component is only rendered on the dashboard page
  *
- * * MainComponent (React Component) : this will be rendered in the content area when a specific route is hit
+ * * Main (React Component) : this will be rendered in the content area when a specific route is hit
  * This component is only rendered when the route is a match.
  * Note : react-router v4 allows you to define nested routes here. TODO: write documentation
  *
- * * StatusComponent (React Component) : this will be rendered on the status page.
+ * * Status (React Component) : this will be rendered on the status page.
  *
  * 4ME CORE uses redux for state management. The API allows the module writer to integrate with the
  * main redux store, which enables devtools to work properly.
@@ -57,11 +64,3 @@ export function getStatusString(state) {
  * must return one of these values : `error`, `warning`, `normal`.
  *
  */
-
-export default {
-  MenuButtonComponent,
-  WidgetComponent,
-  MainComponent,
-  StatusComponent,
-  getReducer,
-};
