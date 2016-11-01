844cc9611971ce8770c3cc6d9289205269c874e6
Benjamin Beret <benjamin.beret@gmail.com>
2016-11-01 13:30:00 +0100

    DevTools will now filter some actions

--- src/dev/DevTools.jsx
+++ src/dev/DevTools.jsx
@@ -4,6 +4,16 @@ import { createDevTools } from 'redux-devtools';
 // Monitors are separate packages, and you can make a custom one
 import LogMonitor from 'redux-devtools-log-monitor';
 import DockMonitor from 'redux-devtools-dock-monitor';
+import FilterMonitor from 'redux-devtools-filter-actions';
+
+import {
+  SET_FETCHER_SERVICES_STATUS,
+  SET_POSITION_SERVICE_STATUS,
+} from '../xman/actions/backend-status';
+
+import {
+  COMPLETE,
+} from '../xman/actions/flight-list';
 
 // createDevTools takes a monitor and produces a DevTools component
 export default createDevTools(
@@ -11,6 +21,14 @@ export default createDevTools(
     toggleVisibilityKey="ctrl-h"
     changePositionKey="ctrl-q"
   >
-    <LogMonitor theme="nicinabox" />
+    <FilterMonitor
+      blacklist={[
+        SET_FETCHER_SERVICES_STATUS,
+        SET_POSITION_SERVICE_STATUS,
+        COMPLETE,
+      ]}
+    >
+      <LogMonitor theme="nicinabox" />
+    </FilterMonitor>
   </DockMonitor>
 );
