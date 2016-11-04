7221731e5b69b5f52d6b2dca24f19a56e9a63dde
Benjamin Beret <benjamin.beret@gmail.com>
2016-11-04 16:18:14 +0100

    Use Material-ui <Badge /> for notification icon in LeftMenu

--- src/core/components/LeftMenu/NotificationIcon.js
+++ src/core/components/LeftMenu/NotificationIcon.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
-import NumericBox from '../icons/NumericBox';
+import Badge from 'material-ui/Badge';
+
 import {
   info,
   warning,
   critical,
 } from '../../../theme/colors';
 
-export const NotificationIcon = ({count, priority = 'info', style, ...rest}) => {
+export const NotificationIcon = ({
+  count,
+  priority = 'info',
+  ...rest
+}) => {
   if(!count) {
     return null;
   }
@@ -22,26 +27,18 @@ export const NotificationIcon = ({count, priority = 'info', style, ...rest}) => 
     color = warning;
   }
 
-  const ComponentSlug = NumericBox[count] || NumericBox['9plus'];
+  const badgeStyle = {
+    backgroundColor: color,
+    fontWeight: 'bolder',
+  };
 
-  // This workaround is here because we render this element as a secondaryText
-  // Material-ui has 2 ways of putting stuff on the right hand side of a MenuItem
-  // First one is to use rightIcon prop, which will place the icon in the right place
-  // but breaks when trying to render anything different
-  // Second one is to use secondaryText prop which works fine for text but breaks for icons
-  // since it relies on line-height to vertically align stuff
-  // This hack will push the rendered icon a little bit down to realign
-  // const materialUiHack = {position: 'relative', top: 6};
-
-  const newStyles = Object.assign(
-    {},
-    style,
-    // materialUiHack,
-    {fill: color},
-  );
 
   return (
-    <ComponentSlug style={newStyles} {...rest} />
+    <Badge
+      badgeContent={count}
+      badgeStyle={badgeStyle}
+      {...rest}
+    />
   );
 };
 
