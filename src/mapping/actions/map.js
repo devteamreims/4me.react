504cadf4ec112b0c3e2b84826ff686da33e3e094
Benjamin Beret <benjamin.beret@gmail.com>
2016-11-05 11:10:24 +0100

    Disabled CWPs will remain disabled on map commit devteamreims/4ME#131

--- src/mapping/actions/map.js
+++ src/mapping/actions/map.js
@@ -105,7 +105,7 @@ export function bindSectorsToCwp(cwpId, sectors) {
     const newMap = _(getMap(getState()))
       .reject(cwp => cwp.cwpId === cwpId)
       .map(removeSectorsFromOtherCwps)
-      .reject(cwp => _.isEmpty(cwp.sectors))
+      .reject(cwp => _.isEmpty(cwp.sectors) && !cwp.disabled)
       .concat(addSectorsToGivenCwp())
       .value();
 
