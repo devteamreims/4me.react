d273e802ca27b843a7eadea6cbc7bf6b301423f0
Benjamin Beret <benjamin.beret@gmail.com>
2016-12-02 23:38:44 +0100

    Use 4me.env library in getPrettifySector

--- src/core/selectors/sectorTree.js
+++ src/core/selectors/sectorTree.js
@@ -1,5 +1,10 @@
 import p from './prefix';
 import _ from 'lodash';
+import R from 'ramda';
+
+import getEnv from '4me.env';
+const { getSectorGroupByElementarySectors } = getEnv('LFEE').sectors;
+
 
 import { createSelector } from 'reselect';
 
@@ -10,24 +15,37 @@ export const isErrored = (state) => !_.isEmpty(_.get(getRaw(state), 'error'));
 
 export const getTree = (state) => _.get(getRaw(state), 'sectorTree', []);
 
-export const getPrettifySectors = createSelector(
-  [getTree],
-  (sectorTree) => _.memoize((sectors = []) => {
-    const sectorComparator = (group1) => (group2) => {
-      const comp1 = _.map(group1, _.toUpper);
-      const comp2 = _.map(group2, _.toUpper);
+export const getPrettifySectors = () => (elementarySectors) => {
+  if(!elementarySectors || R.isEmpty(elementarySectors)) {
+    return '';
+  }
 
-      return _.isEqual(_.sortBy(comp1), _.sortBy(comp2));
-    };
+  const sectorGroup = getSectorGroupByElementarySectors(elementarySectors);
 
-    const equalsToGivenSectors = sectorComparator(sectors);
-
-    const item = _.find(sectorTree, (treeSector) => equalsToGivenSectors(_.get(treeSector, 'elementarySectors', [])));
-    return _.get(item, 'name', sectors.join(','));
-  })
-);
+  return (
+    sectorGroup && sectorGroup.name
+  ) || elementarySectors.join(',');
+};
 
 /* eslint-disable */
+// export const getPrettifySectors = createSelector(
+//   [getTree],
+//   (sectorTree) => _.memoize((sectors = []) => {
+//     const sectorComparator = (group1) => (group2) => {
+//       const comp1 = _.map(group1, _.toUpper);
+//       const comp2 = _.map(group2, _.toUpper);
+//
+//       return _.isEqual(_.sortBy(comp1), _.sortBy(comp2));
+//     };
+//
+//     const equalsToGivenSectors = sectorComparator(sectors);
+//
+//     const item = _.find(sectorTree, (treeSector) => equalsToGivenSectors(_.get(treeSector, 'elementarySectors', [])));
+//     return _.get(item, 'name', sectors.join(','));
+//   })
+// );
+
+
 // export const getPrettifySectors = (state) => (sectors = []) => {
 //   const sectorComparator = (group1) => (group2) => {
 //     const comp1 = _.map(group1, _.toUpper);
