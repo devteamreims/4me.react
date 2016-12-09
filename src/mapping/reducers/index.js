48db59a67a2eefa741b606e61d7b18a965722f9d
Benjamin Beret <benjamin.beret@gmail.com>
2016-12-09 16:53:31 +0100

    Use `4me.env` to provide suggestions in CONTROL ROOM

--- src/mapping/reducers/index.js
+++ src/mapping/reducers/index.js
@@ -3,14 +3,12 @@ import { combineReducers } from 'redux';
 import cwp from './cwp';
 import map from './map';
 import status from './status';
-import suggest from './suggest';
 import dialog from './dialog';
 
 
 const rootReducer = combineReducers({
   cwp,
   map,
-  suggest,
   dialog,
   status,
 });
