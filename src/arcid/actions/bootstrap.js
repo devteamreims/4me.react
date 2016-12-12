8ba7872ba436f612c9c2a920bddaf33ff61472de
Benjamin Beret <benjamin.beret@gmail.com>
2016-12-12 10:06:02 +0100

    Lint

--- src/arcid/actions/bootstrap.js
+++ src/arcid/actions/bootstrap.js
@@ -8,10 +8,6 @@ import {
   disconnect,
 } from '../socket';
 
-import {
-  refreshHistory,
-} from './history';
-
 export function bootstrap() {
   return (dispatch) => {
     console.log('Bootstrapping ARCID !!');
