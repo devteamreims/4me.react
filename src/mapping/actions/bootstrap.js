b2e298617573634ee86ec387583be7502acdd051
Benjamin Beret <benjamin.beret@gmail.com>
2016-12-19 22:29:28 +0100

    Refactor control room socket + flowtypes

--- src/mapping/actions/bootstrap.js
+++ src/mapping/actions/bootstrap.js
@@ -1,41 +1,31 @@
+// @flow
 import { refreshMap } from './map';
-
-import io from 'socket.io-client';
-import api from '../../api';
-
 import {
-  setupSocketIo,
-  getSocket,
-  disconnect,
-} from '../socket';
+  connectSocket,
+  disconnectSocket,
+} from './socket';
 
-export function bootstrap() {
+import type {
+  ThunkAction,
+} from '../../store';
+
+export function bootstrap(): ThunkAction<*> {
   return (dispatch) => {
     console.log('Bootstrapping MAPPING !!');
-    // Fetch CWPs and fetch map
-
-    const setupSocket = () => {
-      if(getSocket()) {
-        console.log('mapping/bootstrap: Bootstrapping mapping while already having a socket.');
-        return;
-      }
-
-      const socketIo = io.connect(api.mapping.socket);
-
-      return setupSocketIo(dispatch, socketIo);
-    };
 
     return Promise.all([
       dispatch(refreshMap()),
-      setupSocket(),
+      dispatch(connectSocket()),
     ]);
   };
 }
 
-export function cleanUp() {
-  return () => {
+export function cleanUp(): ThunkAction<*> {
+  return (dispatch) => {
     console.log('Cleaning up MAPPING');
 
-    disconnect();
+
+
+    dispatch(disconnectSocket());
   };
 }
