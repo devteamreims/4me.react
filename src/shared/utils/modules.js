e4759b5a4d4ab05a0d11bd28cf5887ed0b22254d
Benjamin Beret <benjamin.beret@gmail.com>
2016-12-27 00:32:01 +0100

    Use our config abstraction in isModuleDisabled

--- src/shared/utils/modules.js
+++ src/shared/utils/modules.js
@@ -1,9 +1,10 @@
 import R from 'ramda';
+import { getKey } from '../config';
 
 export const isModuleDisabled = (name) => {
   if(!name) {
     throw new Error('Please submit a valid name');
   }
-  const disabledModules = R.propOr([], 'disabledModules', window.FOURME_CONFIG);
+  const disabledModules = getKey('disabledModules') || [];
   return R.contains(name, disabledModules);
 };
