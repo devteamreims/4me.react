b6d13570304a8ca76f39177ae2653a7a37c8c6da
Benjamin Beret <benjamin.beret@gmail.com>
2017-02-11 13:11:43 +0100

    Fix weird behaviour with flow & reselect

--- src/core/selectors/client.js
+++ src/core/selectors/client.js
@@ -3,7 +3,7 @@ import p from './prefix';
 import _ from 'lodash';
 import { createStructuredSelector } from 'reselect';
 
-import type { Selector } from '../../store';
+import type { RootState, Selector } from '../../store';
 import type { State } from '../reducers/client';
 
 import type {
@@ -17,9 +17,9 @@ export const getRaw: Selector<State> = state => _.get(p(state), 'client', {});
 export const isLoading: Selector<boolean> = state => !!_.get(getRaw(state), 'isLoading');
 export const isErrored: Selector<boolean> = state => !_.isEmpty(_.get(getRaw(state), 'error'));
 
-export const getClientId: Selector<?ClientId> = state => _.get(getRaw(state), 'client.id');
-export const getClientType: Selector<?ClientType> = state => _.get(getRaw(state), 'client.type');
-export const getClientName: Selector<?string> = state => _.get(getRaw(state), 'client.name', '');
+export const getClientId = (state: RootState): ?ClientId => _.get(getRaw(state), 'client.id');
+export const getClientType = (state: RootState): ?ClientType => _.get(getRaw(state), 'client.type');
+export const getClientName = (state: RootState): ?string => _.get(getRaw(state), 'client.name', '');
 
 export const getClient: Selector<?Client> = createStructuredSelector({
   id: getClientId,
